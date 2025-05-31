0ddf97176e68b257b64b654d4907f13a294480c6
RODRIGOMORAMO <rmora@hcgestion.cl>
2025-05-31 10:17:16 -0400

    prueba

--- marketplace-frontend/vite.config.js
+++ marketplace-frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/marketplace/', // 👈 Muy importante para GitHub Pages
+  base: '/marketplace-frontend/', // 👈 Muy importante para GitHub Pages
   plugins: [react()],
 });
 
