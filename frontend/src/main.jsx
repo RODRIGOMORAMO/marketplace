cd8d9d6ac7230ed4d03c53b69aec349cb165d790
RODRIGOMORAMO <rmora@hcgestion.cl>
2025-06-19 23:30:23 -0400

    se actualiza main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import UserProvider from './context/UserContext';
-import PublicacionesProvider from './context/PublicacionesContext';
+import { UserProvider } from './context/UserContext'; // ✅ CORREGIDO
+import { PublicacionesProvider } from './context/PublicacionesContext';
 import './index.css';
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
