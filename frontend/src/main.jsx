fbacd3d46217356eadc9573760a6805b5561cca7
RODRIGOMORAMO <rmora@hcgestion.cl>
2025-06-21 11:58:47 -0400

    import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { UserProvider } from './context/UserContext'; // ✅ CORREGIDO
-import { PublicacionesProvider } from './context/PublicacionesContext';
+import PublicacionesProvider from './context/PublicacionesContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
